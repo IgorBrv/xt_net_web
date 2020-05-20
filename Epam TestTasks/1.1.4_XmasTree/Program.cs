08e5abfef4eb01807e0099360f25561f94d0281a
IgorBrv <65345863+IgorBrv@users.noreply.github.com>
2020-05-20 17:37:49 +0400

    task 1.1.3 done

--- Epam TestTasks/1.1.4_XmasTree/Program.cs
+++ Epam TestTasks/1.1.4_XmasTree/Program.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Triangle
+namespace XmasTree
 {  // Написать программу, которая запрашивает с клавиатуры число N и выводит на экран изображение равностороннего треугольника, состоящее из N строк:
 	class Program
 	{
@@ -19,23 +20,23 @@ namespace Triangle
 			{
 				Console.BackgroundColor = ConsoleColor.Green;
 				Console.ForegroundColor = ConsoleColor.Black;
-				Console.WriteLine("\n ОТРИСОВКА ТРЕУГОЛЬНИКА ИЗ ЗАДАННОГО КОЛЛИЧЕСТВА СТРОК \n");
+				Console.WriteLine("\n ОТРИСОВКА ЁЛКИ ИЗ ЗАДАННОГО КОЛЛИЧЕСТВА ЭЛЕМЕНТОВ \n");
 				Console.ResetColor();
 				//Введём несколько уточняющих принтов отображающихся в случае некорректного ввода
 				if (error == 1)
 				{
 					Console.ForegroundColor = ConsoleColor.Red;
 					Console.Write("К вводу допускаются целые положительные числа [0 < вводимое число < 256] или фраза 'exit'!!!\n");
-					Console.WriteLine("\n24 строка полностью умещаются в окно командной строки стандартного размера.");
-					Console.WriteLine("Треугольник размером до 60 строк корректно отрисовывается в окне командной строки стандартного размера.");
+					Console.WriteLine("\nЁлка из 6 элементов полностью умещается в окно командной строки стандартного размера.");
+					Console.WriteLine("Ёлка размером до 58 элементов корректно отрисовывается в окне командной строки стандартного размера.");
 					Console.ResetColor();
 				}
 				else if (error == 0)
 				{
-					Equi_Triangle triangle = new Equi_Triangle(num);
-					triangle.Draw();
+					XmasTree triangle = new XmasTree(num);
+					triangle.Draw_Numbered();
 				}
-				Console.WriteLine("\nВведите \"exit\" для выхода ИЛИ желаемое колличество строк для ОТРИСОВКИ треугольника:\n");
+				Console.WriteLine("\nВведите \"exit\" для выхода ИЛИ желаемое колличество элементов для ОТРИСОВКИ ёлки:\n");
 				str = Console.ReadLine().Trim();
 				// Осуществим предварительную проверку ввода на пустую строку или на наличие команды выхода:
 				if (str.ToLower() == "exit") { Console.Clear(); break; }
@@ -51,21 +52,41 @@ namespace Triangle
 
 		}
 	}
-	class Equi_Triangle
+	class XmasTree
 	{
 		private byte strings;  // Колличество строк треугольника
-		public Equi_Triangle(byte snum)
+		public XmasTree(byte snum)
 		{
 			strings = snum;
 		}
 		public void Draw()
-		{
+		{  // Метод простой отрисовки ёлки
 			Console.ForegroundColor = ConsoleColor.Green;
-			for (byte i = 0; i < strings; i++)
+			for (int x = 1; x <= strings; x++)
 			{
-				string str = new string('*', 1 + i*2);
-				string filler = new string(' ', strings-1-(str.Length-1)/2);
-				Console.WriteLine(filler + str);
+				for (byte i = 0; i < x; i++)
+				{
+					string str = new string('*', 1 + i * 2);
+					string filler = new string(' ', strings - 1 - (str.Length - 1) / 2);
+					Console.WriteLine(filler + str);
+				}
+			}
+			Console.ResetColor();
+		}
+		public void Draw_Numbered()
+		{  // Метод отрисовки ёлки с пронумероваными элементами
+			Console.ForegroundColor = ConsoleColor.Green;
+			for (int x = 1; x <= strings; x++)
+			{
+				for (byte i = 0; i < x; i++)
+				{
+					string str = new string('*', 1 + i * 2);
+					string filler = "";
+					string num = "   | ";
+					filler = new string(' ', strings - 1 - (str.Length - 1) / 2);
+					if (i == 0) { num = $"{x}| ".PadLeft(5); }
+					Console.WriteLine(num + filler + str);
+				}
 			}
 			Console.ResetColor();
 		}
