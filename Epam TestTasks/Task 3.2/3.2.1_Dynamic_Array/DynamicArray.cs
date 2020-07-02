3dda6858c5f1696c05067b17ad5266a48dba56e6
IgorBrv <65345863+IgorBrv@users.noreply.github.com>
2020-07-02 17:50:56 +0400

    Test solution

---
+++ Epam TestTasks/Task 3.2/3.2.1_Dynamic_Array/DynamicArray.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Array
+{
+    public class DynamicArray<T> : IList<T>, ICloneable
+    {
+        private T[] baseArray;
+        private int count = 0;
+        private bool readOnly = false;
+
+        public DynamicArray()
+        {
+            baseArray = new T[8];
+            Console.WriteLine(baseArray.Length);
+        }
+
+        public DynamicArray(int capacity)
+        {
+            baseArray = new T[capacity];
+            Console.WriteLine(baseArray.Length);
+        }
+
+        public DynamicArray(IEnumerable<T> collection)
+        {
+            baseArray = collection.ToArray();
+            this.count = collection.Count();
+        }
+
+        private DynamicArray(T[] collection, int count)
+        {
+            baseArray = new T[collection.Length];
+            Array.Copy(collection, baseArray, collection.Length);
+            this.count = count;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (count != 0 && index < count)
+                {
+                    return baseArray[index];
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            set
+            {
+                baseArray[index] = value;
+            }
+        }
+
+        public int Count => count;
+
+        public bool IsReadOnly => readOnly;
+
+        public void Add(T item)
+        {
+            if (count >= baseArray.Length)
+            {
+                T[] tempArray = new T[baseArray.Length*2];
+                for (int i = 0; i < count; i++)
+                {
+                    tempArray[i] = baseArray[i];
+                }
+                baseArray = tempArray;
+            }
+            baseArray[count] = item;
+            count++;
+        }
+
+        public void Clear()
+        {
+            baseArray = new T[8];
+            count = 0;
+        }
+
+        public object Clone()
+        {
+            return new DynamicArray<T>(baseArray, count);
+        }
+
+
+        public bool Contains(T item)
+        {
+            return baseArray.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (IEnumerator<T>)baseArray.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Epam TestTasks/Task 3.2/3.2.1_Dynamic_Array_Demonstration/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynamic_Array;
+
+namespace Dynamic_Array_Demonstration
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> list = new List<string> { "Один", "два" };
+			list.AsReadOnly();
+			int coynt = list.Count;
+
+			int[] array = new int[3] { 1, 2, 3 };
+
+			int?[] array2 = new int?[3];
+
+
+			Console.WriteLine("!!!" + ReferenceEquals(array, array2));
+
+
+			Console.WriteLine(string.Join(", ", array));
+			Console.WriteLine(string.Join(", ", array2));
+
+			list[1] = "123";
+			foreach (string i in list)
+			{
+				Console.WriteLine(i);
+			}
+
+			string[] array3 = new string[3] { "один", "два", "три" };
+
+			DynamicArray<string> da = new DynamicArray<string>(array3);
+			DynamicArray<string> da2 = new DynamicArray<string>();
+			da.Add("четыре");
+			da2 = (DynamicArray<string>)da.Clone();
+			Console.WriteLine("!!!!!" + da[3]);
+			Console.WriteLine("!!!!!" + da2[3]);
+			Console.WriteLine("!!!!!" + da2.Contains("четыре"));
+
+			Test<string> tst = new Test<string>("Один", "Два", "Три");
+
+			foreach (string i in tst)
+			{
+				Console.WriteLine(i);
+			}
+
+			Console.ReadLine();
+		}
+	}
+
+	class Test<T> : IEnumerator<T>, IEnumerable<T>
+	{
+
+
+
+		T[] baseArray = new T[3];
+
+		public Test(T a, T b, T c)
+		{
+			baseArray[0] = a;
+			baseArray[1] = b;
+			baseArray[2] = c;
+		}
+
+		private T item;
+		private int curIndex = -1;
+
+
+		public T Current => item;
+
+		object IEnumerator.Current => Current;
+
+		public void Dispose()
+		{
+			// На docs.microsoft в данной ситуации сказанно оставить пустую реализацию
+		}
+
+		public bool MoveNext()
+		{
+			if (++curIndex >= baseArray.Length)
+			{
+				return false;
+			}
+			else
+			{
+				item = baseArray[curIndex];
+			}
+			return true;
+		}
+
+		public void Reset()
+		{
+			curIndex = -1;
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			Console.WriteLine("!!! это");
+			return this;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return baseArray.GetEnumerator();
+		}
+	}
+}
