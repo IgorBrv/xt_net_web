0b5ba712da472d13f8e8bf8f318a79e96dc9e143
IgorBrv <65345863+IgorBrv@users.noreply.github.com>
2020-07-06 17:57:15 +0400

    Task 3.1 rebuild

--- Epam TestTasks/Task 3.1/Task 3.1.1/Human.cs
+++ Epam TestTasks/Task 3.1/Task 3.1.1/Human.cs
@@ -4,8 +4,14 @@ namespace Task_3_1_1
 {
 	class Human
 	{	// Класс описывающий участника "Слабого звена
+
 		private string name;
 		private bool active = true;
+
+		public Human(int num)
+		{
+			name = $"Человек {num}\n";
+		}
 		public bool Active
 		{
 			get
@@ -24,11 +30,6 @@ namespace Task_3_1_1
 					active = value;
 				}
 			}
-		}	
-
-		public Human(int num)
-		{
-			name = $"Человек {num}\n";
 		}
 
 		public override string ToString()
