84c574a67bba5bb13741a3b59ae119cff6ab18fd
IgorBrv <65345863+IgorBrv@users.noreply.github.com>
2020-07-14 13:54:34 +0400

    task3.2 unusual_situation_demo

---
+++ Epam TestTasks/Task 3.2/3.2.1_DynamicArrayDemonstration_ForTests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamicArrayLib;
+
+namespace DynamicArrayDemonstration_ForTests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> lst = new List<int> { 1, 2, 3 };
+			DynamicArray<int> da1 = new DynamicArray<int>(lst);
+			DynamicArray<int> da2 = new DynamicArray<int>(da1);
+			CycledDynamicArray<int> cda1 = new CycledDynamicArray<int>(da1);
+			Console.ReadLine();
+		}
+	}
+}
